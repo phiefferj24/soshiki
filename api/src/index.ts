adcf7807b88c9afebc352c0cba37e41924807809
Jim Phieffer <phiefferj24@columbusacademy.org>
2022-11-14 13:02:07 -0500

    Added PATCH method to CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -48,7 +48,7 @@ const cors = (req: any, res: any, next: any) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.header('Access-Control-Allow-Credentials', 'true');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
         next();
         return;
     }
@@ -59,7 +59,7 @@ const cors = (req: any, res: any, next: any) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 }
 
